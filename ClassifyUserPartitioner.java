3f4e854e98a13f862740d73be42a12717ac00486
Shradha P. Joshi <shradhapj@gmail.com>
2017-06-25 16:08:56 +0530

    Update ClassifyUserPartitioner.java

--- ClassifyUserPartitioner.java
+++ ClassifyUserPartitioner.java
@@ -11,15 +11,15 @@ public class UserPartitioner extends Partitioner<Text, IntWritable>{
 		String change_size = value.toString();
 		int chage_Size_Int = Math.abs(Integer.parseInt(change_size));
 		
-		//this is done to avoid performing mod with 0
+		
 		if(numReduceTasks == 0)
 			return 0;
 
-		//if the age is <20, assign partition 0
+		
 		if(chage_Size_Int <=12){				
 			return 0;
 		}
-		//else if the age is between 20 and 50, assign partition 1
+		
 		if(chage_Size_Int >12 && chage_Size_Int <=50){
 			
 			return 1;
